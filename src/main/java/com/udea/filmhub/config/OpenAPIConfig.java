db6dbeaa697bc51d374c0584589ada8679ddce76
Ricardo Contreras Garzón <102176435+RickContreras@users.noreply.github.com>
2024-10-01 04:25:26 +0000

    ✨ Añade soporte para Swagger en Codespaces
    - Se habilitó la funcionalidad para usar Swagger en Codespaces.
    - Es importante cambiar ´swagger.server.url´ en el archivo ´application-dev.properties´ por la URL de su Codespaces, como se menciona en el README.

--- src/main/java/com/udea/filmhub/config/OpenAPIConfig.java
+++ src/main/java/com/udea/filmhub/config/OpenAPIConfig.java
@@ -1,6 +1,5 @@
 package com.udea.filmhub.config;
 
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,10 +7,7 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import java.util.List;
-
-
 
 @Configuration
 public class OpenAPIConfig {
