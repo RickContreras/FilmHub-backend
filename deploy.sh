59f23346376df9be33bc63de72c1d15f8767c0c9
RickContreras <ricardo.contreras568@hotmail.com>
2024-10-03 23:08:34 -0500

    🔨 refactor: Renombrar y corregir archivos
    - Actualizado la configuración de la aplicacion web en ´application-pnd.properties

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ az appservice plan create --name filmhub-backend-plan --resource-group $AZ_RESOU
 
 # Crear aplicación web
 echo "Creando aplicación web"
-az webapp create --resource-group $AZ_RESOURCE_GROUP --plan filmhub-backend-plan --name filmhub-backend-app --runtime "JAVA|21-java21"
+az webapp create --resource-group $AZ_RESOURCE_GROUP --plan filmhub-backend-plan --name filmhub-backend --runtime "JAVA|21-java21"
 
 # Configurar variables de entorno en la aplicación web
 echo "Configurando variables de entorno en la aplicación web"
-az webapp config appsettings set --resource-group $AZ_RESOURCE_GROUP --name filmhub-backend-app --settings \
+az webapp config appsettings set --resource-group $AZ_RESOURCE_GROUP --name filmhub-backend --settings \
     AZ_RESOURCE_GROUP=$AZ_RESOURCE_GROUP \
     AZ_DATABASE_NAME=$AZ_DATABASE_NAME \
     AZ_LOCATION=$AZ_LOCATION \
@@ -21,13 +21,13 @@ az webapp config appsettings set --resource-group $AZ_RESOURCE_GROUP --name film
     SPRING_DATASOURCE_PASSWORD=$SPRING_DATASOURCE_PASSWORD
 
 # Empaquetar la aplicación
-echo "Empaquetando la aplicación"
-./mvnw clean package
+#echo "Empaquetando la aplicación"
+#./mvnw clean package
 
 # Desplegar la aplicación
-echo "Desplegando la aplicación"
-az webapp deploy --resource-group $AZ_RESOURCE_GROUP --name filmhub-backend-app --src-path target/filmhub-0.0.1-SNAPSHOT.jar --type jar
+#echo "Desplegando la aplicación"
+#az webapp deploy --resource-group $AZ_RESOURCE_GROUP --name filmhub-backend --src-path target/filmhub-0.0.1-SNAPSHOT.jar --type jar
 
 # Verificar el despliegue
-echo "Verificando el despliegue"
-echo "Abre tu navegador y navega a https://filmhub-backend-app.azurewebsites.net para verificar que tu aplicación esté funcionando"
+#echo "Verificando el despliegue"
+#echo "Abre tu navegador y navega a https://filmhub-backend.azurewebsites.net para verificar que tu aplicación esté funcionando"
