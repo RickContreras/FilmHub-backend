e5a281cf8a9475856bb24a5cdd0c48e93fab1998
Ricardo Contreras Garzón <102176435+RickContreras@users.noreply.github.com>
2025-02-02 01:24:37 +0000

    📝 Documentar todos los DTOs en el proyecto
    - Añadir anotaciones de Swagger a GeneroDTO.
    - Documentar GeneroXUsuarioResponse con descripciones y ejemplos.

--- src/main/java/com/udea/filmhub/dto/GeneroDTO.java
+++ src/main/java/com/udea/filmhub/dto/GeneroDTO.java
@@ -1,9 +1,12 @@
 package com.udea.filmhub.dto;
-
+import io.swagger.v3.oas.annotations.media.Schema;
+@Schema(description = "Detalles del género")
 public class GeneroDTO {
     
     //En general no deberia ir el id en el DTO, pero para efectos de este ejemplo lo dejaremos
+    @Schema(description = "ID del género", example = "1")
     private Long id;
+    @Schema(description = "Nombre del género", example = "Accion")
     private String nombre;
 
     // Getters y Setters
